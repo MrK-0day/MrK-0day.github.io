7f642dbd113f81aac51a4527b5e7deeb7466caa0
MrK-0day <33540741+MrK-0day@users.noreply.github.com>
2018-01-30 11:00:17 +0700

    Commit 1

---
+++ libraries/app.js
@@ -0,0 +1,15 @@
+var config = {
+    MrK_Icon_Link : './icon.png'
+};
+var App_Head = new Vue({
+    el : '#App_Head',
+    data : {
+        Icon_Link : config['MrK_Icon_Link']
+    }
+});
+var App_Body = new Vue({
+    el : '#App_Body',
+    data : {
+        
+    }
+});
