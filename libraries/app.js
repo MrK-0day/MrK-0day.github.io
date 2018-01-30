d968d01b2792d88f2b4a1406e14d8e0c61d914f1
MrK-0day <33540741+MrK-0day@users.noreply.github.com>
2018-01-30 15:57:20 +0700

    commit 2

--- libraries/app.js
+++ libraries/app.js
@@ -1,15 +1,36 @@
-var config = {
-    MrK_Icon_Link : './icon.png'
-};
-var App_Head = new Vue({
-    el : '#App_Head',
-    data : {
-        Icon_Link : config['MrK_Icon_Link']
+function member() {
+    this.user = '';
+    this.pass = '';
+    this.set = function(u, p) {
+        this.user = u;
+        this.pass = p;
+    };
+    this.login = function() {
+        return 'concac';
+    };
+    this.register = function() {
+        return 'concu';
     }
-});
+    return this;
+}
 var App_Body = new Vue({
     el : '#App_Body',
     data : {
-        
+        username : '',
+        password : ''
+    },
+    methods : {
+        load : function(req) {
+            var res = new member();
+            res.set(this.username, this.password);
+            if (req == 'login') {
+                res.login();
+            } else if (req == 'register') {
+                res.register();
+            }
+        }
     }
+});
+$('#login_button').click(function(){
+    $('.ui.basic.modal').modal('show');
 });
---
+++ libraries/config.js
@@ -0,0 +1,9 @@
+var config = {
+    MrK_Icon_Link : './icon.png'
+};
+var App_Head = new Vue({
+    el : '#App_Head',
+    data : {
+        Icon_Link : config['MrK_Icon_Link']
+    }
+});
